2fe2bee1821f2ce9cc747ef64ad34082b6a917e0
giddeladewoye20-droid <giddeladewoye20@gmail.com>
2025-09-13 17:47:51 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 import pgzrun
-from random import randint
+import random
 
 WIDTH=500
 HEIGHT=500
 TITLE="PAQUETA CRISPS"
 
 alien=Actor("alien")
+alien.pos=250,250
+score=0
 msg=""
 
 def draw():
@@ -14,26 +16,19 @@ def draw():
     screen.fill("lime")
 
     alien.draw()
-    screen.draw.text(msg,center=(400,20),fontsize=30,color="orange")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    screen.draw.text(msg,center=(250,250),fontsize=30,color="purple")
+def on_mouse_down(pos): 
+    global msg
+    global score
+    if alien.collidepoint(pos):
+        alien.pos=random.randint(0,500), random.randint(0,500)
+        score= score + 1
+        msg="You have hit the Alien!You now have " + str (score)+"points"
+        
+    else:
+        score= score - 1
+        msg="You have not hit the Alien! You now have"+ str(score) + "points"
+       
 
 
 
